547fca3844f70b97cf9ad329f13a1054d114e357
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-11-22 14:59:30 +0700

    .

--- components/User/Doctor/body.vue
+++ components/User/Doctor/body.vue
@@ -40,9 +40,7 @@
         >
           <button
             type="button"
-            id="updateDoctorButton"
-            data-modal-target="updateDoctor"
-            data-modal-toggle="updateDoctor"
+            
             class="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
           >
             <svg
@@ -144,10 +142,8 @@
                   <li>
                     <button
                       type="button"
-                      id="updateDoctorButton"
-                      data-modal-target="updateDoctor"
-                      data-modal-toggle="updateDoctor"
-                      class=" py-2 px-4 w-full flex items-start justify-start hover:bg-gray-100"
+                      
+                      class="py-2 px-4 w-full flex items-start justify-start hover:bg-gray-100"
                     >
                       Chỉnh sửa
                     </button>
