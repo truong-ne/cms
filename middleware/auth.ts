f9a4bc5dee2b1465833680ea34e18759336e2f0d
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-11-25 00:09:34 +0700

    no mesage

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,20 +1,20 @@
 import { useAuthStore } from "@/stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  // try {
-  const authStore = useAuthStore();
-  if (authStore.accessToken != "") {
-    if (to.path == "/authenticate") {
-      return navigateTo("/");
+  try {
+    const authStore = useAuthStore();
+    if (authStore.getAccesToken != "") {
+      if (to.path == "/authenticate") {
+        return navigateTo("/");
+      }
+      return;
+    } else {
+      if (to.path != "/authenticate") {
+        return navigateTo("/authenticate");
+      } else return;
     }
-    return;
-  } else {
-    if (to.path != "/authenticate") {
-      return navigateTo("/authenticate");
-    } else return;
+  } catch (e) {
+    console.log(e);
+    navigateTo("/authenticate");
   }
-  // } catch (e) {
-  //   console.log(e);
-  //   navigateTo("/authenticate");
-  // }
 });
--- stores/structs/response_struct.ts
+++ stores/structs/response_struct.ts
@@ -1,36 +1,46 @@
-import { string, number, array, record, object, size, optional, defaulted, assign, type Infer } from 'superstruct'
-
+import {
+  string,
+  number,
+  array,
+  record,
+  object,
+  size,
+  optional,
+  defaulted,
+  assign,
+  type Infer,
+} from "superstruct";
 
 export const DataObjectSchema = object({
   code: number(),
   message: string(),
-  data: object()
-})
+  data: object(),
+});
 
 export const DataObjectLoginSchema = object({
-  data: object()
-})
+  data: object(),
+});
 
 export const DataArraySchema = object({
   code: number(),
   message: string(),
-  data: array()
-})
+  data: array(),
+});
 
 export const DataStringSchema = object({
   code: number(),
   message: string(),
-  data: string()
-})
+  data: string(),
+});
 
 export const DataNumberSchema = object({
   code: number(),
   message: string(),
-  data: number()
-})
+  data: number(),
+});
 
-export type DataObject = Infer<typeof DataObjectSchema>
-export type DataArray = Infer<typeof DataArraySchema>
-export type DataString = Infer<typeof DataStringSchema>
-export type DataNumber = Infer<typeof DataNumberSchema>
-export type DataObjectLogin = Infer<typeof DataObjectLoginSchema>
+export type DataObject = Infer<typeof DataObjectSchema>;
+export type DataArray = Infer<typeof DataArraySchema>;
+export type DataString = Infer<typeof DataStringSchema>;
+export type DataNumber = Infer<typeof DataNumberSchema>;
+export type DataObjectLogin = Infer<typeof DataObjectLoginSchema>;
