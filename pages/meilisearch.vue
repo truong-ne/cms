05c1ec5bd24c4beb3c42f3bf9758a016bbc97f0e
truongne <truonggolang@gmail.com>
2023-12-02 23:16:36 +0700

    dropdown to button

--- pages/meilisearch.vue
+++ pages/meilisearch.vue
@@ -1,8 +1,11 @@
 <script setup>
-const { search, result } = useMeiliSearch('mock')
+const { search, result } = useMeiliSearch('discount')
 
 const searchData = ref('')
 const input = ref('')
+const component = ref(false)
+
+const data = ref('')
 
 async function meilisearch() {
     if (input.value.trim() !== '') {
@@ -12,9 +15,17 @@ async function meilisearch() {
         searchData.value = '';
     }
 }
+
+async function test(id) {
+    data.value = {
+        id: id,
+    }
+    component.value = true
+}
 </script>
 
 <template>
+    <ToastTest v-show="component" :data="data" />
     <section class="bg-gray-50 dark:bg-gray-900 p-3">
         <div class="mx-auto max-w-screen-xl px-4">
             <!-- Start coding here -->
@@ -151,8 +162,8 @@ async function meilisearch() {
                                 </th>
                             </tr>
                         </thead>
-                        <tbody v-for="product in searchData">
-                            <tr class="border-b dark:border-gray-700">
+                        <tbody>
+                            <tr class="border-b dark:border-gray-700" v-for="product in searchData">
                                 <th scope="row"
                                     class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {{ product.id }}</th>
@@ -162,8 +173,9 @@ async function meilisearch() {
                                 <td class="px-4 py-3">{{ product.stock }}</td>
                                 <td class="px-4 py-3">{{ product.rating }}</td>
                                 <td class="px-4 py-3 flex items-center justify-end">
-                                    <button id="benq-ex2710q-dropdown-button" data-dropdown-toggle="benq-ex2710q-dropdown"
-                                        class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+                                    <button :id="'benq-ex2710q-dropdown-button' + product.id" @click="test(product.id)"
+                                        :data-dropdown-toggle="'benq-ex2710q-dropdown' + product.id"
+                                        class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 focus:outline-none hover:text-red-500"
                                         type="button">
                                         <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
                                             xmlns="http://www.w3.org/2000/svg">
@@ -171,24 +183,6 @@ async function meilisearch() {
                                                 d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                         </svg>
                                     </button>
-                                    <div id="benq-ex2710q-dropdown"
-                                        class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                                        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                            aria-labelledby="benq-ex2710q-dropdown-button">
-                                            <li>
-                                                <a href="#"
-                                                    class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
-                                            </li>
-                                            <li>
-                                                <a href="#"
-                                                    class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                                            </li>
-                                        </ul>
-                                        <div class="py-1">
-                                            <a href="#"
-                                                class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
-                                        </div>
-                                    </div>
                                 </td>
                             </tr>
                         </tbody>
