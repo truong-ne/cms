69933dd77eb29658bbf5de85bde8736ad00ed841
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-12-09 08:34:28 +0700

    .

--- components/Dashboard/quickview.vue
+++ components/Dashboard/quickview.vue
@@ -154,9 +154,6 @@
                   class="w-10 h-10 rounded"
                   :alt="blog.full_name"
                 />
-                <span
-                  class="absolute bottom-0 left-8 transform translate-y-1/4 w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"
-                ></span>
               </a>
               <div class="flex-1 w-full relative">
                 <a
